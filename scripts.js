a8accbf3f0c5f99e27e8af2e79f49ce06fd5aab5
PinhoOn <pinhoonjob@gmail.com>
2021-04-06 16:45:03 -0300

    trying my best

--- scripts.js
+++ scripts.js
@@ -1,37 +1,50 @@
 var backColor = ["#833E3E","#3E4D83","#50833E","#793E83","#3E8383","#837C3E"]
 var fontColor = ["#D6DDA8","#DDA8B2","#A8D0DD","#BFDDA8","#D6A8DD","#FFAAAA"]
-var names = ["Banguela" , "AAAA", "BBBB", "CCCC", "DDDD", "EEEE"]
 var contents = [
-  "surgiu dos escravos africanos que vinham da região de Benguela na Angola e arrancavam os seus dentes para identificação na hora da venda.",
-  "aaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa",
-  "bbbbbbb bbbbbbbbbbbbbbb bbbbbbbbbbbbbb bbbbbbbbbbbb bbbbbbbbbbbb",
-  "cccccccccc ccccccccccc cccccccccccc ccccc cccccccc ccccccccccccc",
-  "dddddd ddddddddddd dddddddddddd ddddd dddddd ddddddd ddddd dddddddd",
-  "eeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeee eeeeeee eeeeee eeeeeeeeeee",
+  "Banguela surgiu dos escravos africanos que vinham da região de Benguela na Angola e arrancavam os seus dentes para identificação na hora da venda.",
+  "AAAA aaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa aaaaaaaaaaaa aaaaaaaaaaaaa",
+  "BBBB bbbbbbb bbbbbbbbbbbbbbb bbbbbbbbbbbbbb bbbbbbbbbbbb bbbbbbbbbbbb",
+  "CCCC cccccccccc ccccccccccc cccccccccccc ccccc cccccccc ccccccccccccc",
+  "DDDD dddddd ddddddddddd dddddddddddd ddddd dddddd ddddddd ddddd dddddddd",
+  "EEEE eeeeeee eeeeeeeeeeeeeeeee eeeeeeeeeeee eeeeeee eeeeee eeeeeeeeeee",
 ]
+
 window.onload = initPage
 function initPage(){
   var calc = parseInt(Math.random() * 6 ) 
-  nameCalc = names[calc]
   contentCalc = contents[calc]
   backColorCalc = backColor[calc]
   fontColorCalc = fontColor[calc]
 
-  var textname = document.getElementById("text--name").innerHTML=`${nameCalc}`
-  var textcontent = document.getElementById("text--content").innerHTML=`${contentCalc}`
-  var backGroundChange = document.getElementById("body__specs").style.backgroundColor=`${backColorCalc}`
-  var fontChage = document.getElementById("text--content").style.color=`${fontColorCalc}`
+  var textName = document.getElementById("text--name").innerHTML=`${contentCalc.split(' ')[0]}`
+  var textContent = document.getElementById("text--content").innerHTML=`${contentCalc}`
+  var backGroundChange = document.getElementById("body").style.backgroundColor=`${backColorCalc}`
+  var fontChange = document.getElementById("text--content").style.color=`${fontColorCalc}`
 }
 
 function newCuriosity(){
   var calc = parseInt(Math.random() * 6 ) 
-  nameCalc = names[calc]
   contentCalc = contents[calc]
   backColorCalc = backColor[calc]
   fontColorCalc = fontColor[calc]
 
-  var textname = document.getElementById("text--name").innerHTML=`${nameCalc}`
-  var textcontent = document.getElementById("text--content").innerHTML=`${contentCalc}`
-  var backGroundChange = document.getElementById("body__specs").style.backgroundColor=`${backColorCalc}`
-  var fontChage = document.getElementById("text--content").style.color=`${fontColorCalc}`
+  var textName = document.getElementById("text--name").innerHTML=`${contentCalc.split(' ')[0]}`
+  var textContent = document.getElementById("text--content").innerHTML=`${contentCalc}`
+  var backGroundChange = document.getElementById("body").style.backgroundColor=`${backColorCalc}`
+  var fontChange = document.getElementById("text--content").style.color=`${fontColorCalc}`
+  var backGroundChange = document.getElementById("container").style.backgroundColor=`${backColorCalc}`
+  
+}      
+function takeShot(){
+  let div = document.getElementById("container")
+  html2canvas(div).then(
+  function(canvas){
+    document
+    .getElementById('screen__preview')
+    .appendChild(canvas)
+  var display = document.getElementById("screen__preview").style.display;
+  if (display == "none"){
+    document.getElementById("screen__preview").style.display = "block";
+  }
+})
 }

54b145b5d326e311ee88c4f1c1954e980a879202
matheusagcosta <matheusagcosta@gmail.com>
2021-04-14 09:21:37 -0300

    Small fixes

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,11 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
+<<<<<<< HEAD
   var ws = new WebSocket(protocol + '://' + hostname + ':' + "44551" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50071" + '/');
+>>>>>>> 895e825... Small fixes
 
   ws.onmessage = function (event) {
     checkedAssets = {};

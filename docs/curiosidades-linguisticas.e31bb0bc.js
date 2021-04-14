f22bc00214ee46fc7431bd497f594e95a0d4d57b
matheusagcosta <matheusagcosta@gmail.com>
2021-04-14 09:28:10 -0300

    Fix scripts to make it work with Github Pags

--- docs/curiosidades-linguisticas.e31bb0bc.js
+++ docs/curiosidades-linguisticas.e31bb0bc.js
@@ -9148,7 +9148,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44551" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50220" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -9325,4 +9325,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js","index.js"], null)
-//# sourceMappingURL=/curiosidades.e31bb0bc.js.map
+//# sourceMappingURL=/curiosidades-linguisticas.e31bb0bc.js.map
--- docs/index.js
+++ docs/index.js
@@ -184,12 +184,7 @@ function reloadCSS() {
 }
 
 module.exports = reloadCSS;
-},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"styles.css":[function(require,module,exports) {
-var reloadCSS = require('_css_loader');
-
-module.hot.dispose(reloadCSS);
-module.hot.accept(reloadCSS);
-},{"_css_loader":"node_modules/parcel-bundler/src/builtins/css-loader.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+},{"./bundle-url":"node_modules/parcel-bundler/src/builtins/bundle-url.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -217,7 +212,15 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40263" + '/');
+<<<<<<< HEAD:dist/index.js
+<<<<<<< HEAD
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44551" + '/');
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50071" + '/');
+>>>>>>> 895e825... Small fixes
+=======
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50220" + '/');
+>>>>>>> b13a27e... Fix scripts to make it work with Github Pags:docs/index.js
 
   ws.onmessage = function (event) {
     checkedAssets = {};
@@ -394,4 +397,4 @@ function hmrAcceptRun(bundle, id) {
   }
 }
 },{}]},{},["node_modules/parcel-bundler/src/builtins/hmr-runtime.js"], null)
-//# sourceMappingURL=/styles.8986bff4.js.map
+//# sourceMappingURL=/index.js.map

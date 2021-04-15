dcb657e39db9c34004d2d59d048a63d7e73e8f0c
PinhoOn <pinhoonjob@gmail.com>
2021-04-15 17:42:29 -0300

    direct link to mail with pre subject

--- docs/curiosidades.e31bb0bc.js
+++ docs/curiosidades.e31bb0bc.js
@@ -9167,7 +9167,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39277" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38391" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
